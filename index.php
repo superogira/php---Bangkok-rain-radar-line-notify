8f70a64b118b11364295caa2f769bd6c58fbe6a3
RNCAT <jaruwat.works@gmail.com>
2021-07-02 09:05:33 +0700

    :heavy_minus_sign: move env to class file

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-include './vendor/autoload.php';
 include './class.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-$RainRadar = new RainRadarNotify($_ENV['LINE_TOKEN']);
+$RainRadar = new RainRadarNotify();
 $RainRadar->setImage('http://weather.bangkok.go.th/FTPCustomer/radar/pics/nkradarh.jpg', 'rainradar1.jpg', 'หนองแขม');
 $RainRadar->setImage('http://weather.bangkok.go.th/FTPCustomer/radar/pics/radarh.jpg', 'rainradar2.jpg', 'หนองจอก');
 $RainRadar->sendNotify();
