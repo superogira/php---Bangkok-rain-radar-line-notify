c161104ee45c39d00f0a795eba21cd30bf594d22
superogira <superogira@hotmail.com>
2021-07-04 22:33:32 +0700

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-include './RainRadar.php';
-
-$RainRadar = new RainRadarNotify();
-$RainRadar->setImage('http://weather.bangkok.go.th/FTPCustomer/radar/pics/nkradarh.jpg', 'rainradar1.jpg', 'หนองแขม');
-$RainRadar->setImage('http://weather.bangkok.go.th/FTPCustomer/radar/pics/radarh.jpg', 'rainradar2.jpg', 'หนองจอก');
-$RainRadar->sendNotify();
